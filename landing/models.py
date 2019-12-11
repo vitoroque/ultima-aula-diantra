56a9d11875e0761c4c01604f2aca2f2b6e41ff0c
diandra <di.andrade90@gmail.com>
2019-12-11 14:52:28 -0300

    First commit

---
+++ landing/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+class Usuario(models.Model):
+    email = models.EmailField(max_length=255, verbose_name='Email', unique=True)
+    senha = models.CharField(max_length=16, verbose_name='Senha')
+
+    def __str__(self):
+        return self.email
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=255, verbose_name='Nome')
+    frase = models.TextField()
+    
+    criado_em =  models.DateTimeField(default=timezone.now)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
